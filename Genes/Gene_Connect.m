7aa86a1388154bb92190b48ac44cb70cecdfc2f9
rysciguy <rysciguy@gmail.com>
2019-04-25 16:39:07 -0600

    Fixing arguments in Gene_Connect

--- Genes/Gene_Connect.m
+++ Genes/Gene_Connect.m
@@ -1,20 +1,23 @@
 classdef Gene_Connect < Gene
     properties
-        id_A;
-        id_B;
+        pt_A_id;
+        pt_B_id;
         stiffness;
+        link_id;
+        
     end
     
     methods
-        function obj = Gene_Connect(i, id_A, id_B, stiffness)
+        function obj = Gene_Connect(i, pt_A_id, pt_B_id, stiffness, link_id)
             obj.innovation = i;
-            obj.id_A = id_A;
-            obj.id_B = id_B;
+            obj.pt_A_id = pt_A_id;
+            obj.pt_B_id = pt_B_id;
             obj.stiffness = stiffness;
+            obj.link_id = link_id;
         end
         function express(obj, bridge)
-            pt_A = bridge.pointID(obj.id_A);
-            pt_B = bridge.pointID(obj.id_B);
+            pt_A = bridge.pointID(obj.pt_A_id);
+            pt_B = bridge.pointID(obj.pt_B_id);
             existing_link = pt_A.connected(pt_B);
             if ~isempty(existing_link)
                 existing_link.enabled = 1;
