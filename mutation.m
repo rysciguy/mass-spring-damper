9121542a4e930a652f8c7618bb939ed3fc606a37
chro3931 <49259643+chro3931@users.noreply.github.com>
2019-04-26 18:28:41 -0600

    Update mutation.m

--- mutation.m
+++ mutation.m
@@ -1,4 +1,4 @@
-function mutation
+%function mutation
 
 %get genome
 %randomly determine which gene to add
@@ -16,7 +16,7 @@ function mutation
 repf = 0;
 ptnum = length(bridges.points); 
 linknum = length(bridges.links); 
-c = 10 %constant that converts repulsion force to a distance the pt moves
+c = 10; %constant that converts repulsion force to a distance the pt moves
 
 
 
@@ -28,13 +28,17 @@ if g == 1
    ng = Gene_Split(n, randlink);
    
 elseif g == 2
-    ng = Gene_Nudge(n, randpt, [randi([-10, 10])/10, randi([-10, 10])/10, 0]);
+    %ng = Gene_Nudge(n, randpt, [randi([-10, 10])/10, randi([-10, 10])/10, 0]);
+    nudge = bridges.points(1,randpt).pos + [randi([-10, 10])/10, randi([-10, 10])/10, 0];
     
 elseif g == 3
-    ng = Gene_Connect();
+    ng = Gene_Connect(n, randi([1,ptnum]), randi([2,ptnum]), 1); %change so that pts are not same
     
 elseif g == 4
-    ng = Gene_Node();
+    ng = Gene_Node(n, [rand.*10, -5+(5+5).*rand, 0]); %change so it matches the x,y lims
+    
+elseif g == 5
+    %change link stiffness
     
 end
 
@@ -44,12 +48,32 @@ if g == 2
     %sum repulsion forces and convert to some distance
     %check to make sure this distance is within the bounds
     %create new ng that equals new nudge
-    origpt = bridges.points(1, randpt);
+    origpt = bridges.points(1, randpt).pos;
     for i = 1:ptnum   
     p = bridges.points(1,i).pos;
-    dist = origpt - p;
-    repf = repf + 1/(dist.^2);
+    dist = origpt + nudge - p;
+    s = dist.^-2;
+    s(1,3) = 0;
+    repf = repf + s;
     end
-    ng = Gene_Nudge(n, randpt, repf/c);
+    repdist = repf/c;
+    check = repdist + origpt;
+    
+    %change these limits to call to x,y limits
+    if check(1,1) > 10 
+        repdist(1,1) = 10 - origpt(1,1);
+    end
+    if check(1,2) > 5
+        repdist(1,2) = 5 - origpt(1,2);
+    end
+    if check(1,1) < 0
+        repdist(1,1) = 0 - origpt(1,1);
+    end
+    if check(1,2) < -5
+        repdist(1,2) = -5 - orgipt(1,2);
+    end
+    
+    ng = Gene_Nudge(n, randpt, repdist);
     
 end
+%end
