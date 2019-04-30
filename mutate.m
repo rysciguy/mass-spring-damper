50e21c2ad0e244c9781ebe3cdcc213fb750ec432
chro3931 <49259643+chro3931@users.noreply.github.com>
2019-04-30 16:28:17 -0600

    Added new node mutation

--- mutate.m
+++ mutate.m
@@ -4,8 +4,9 @@ num_genes = length(g);
 
 p_stiffen = 0.25;
 p_nudge = 0.1;
-p_toggle = 0.05/num_genes;
-p_split = 0.25/num_genes;
+p_toggle = 0/num_genes;
+p_split = 0/num_genes; %0.25
+p_newnode = 0.25/num_genes;
 
 weight_range = [0, 5];
 weight_radius = 2;
@@ -51,6 +52,28 @@ for i = 1:num_genes
         second = Gene_Link(B_id, B_pos, C_id, C_pos, new_stiffness);
         g{length(g)+1} = first;
         g{length(g)+1} = second;
+    end
+    if rand()<p_newnode
+        %g{i}.enabled = false;
+        old_stiffness = g{i}.stiffness;
+        new_stiffness = old_stiffness; %may change later
+        
+        A_pos = g{i}.A_pos; A_id = g{i}.A_id;
+        B_pos = g{i}.B_pos; B_id = g{i}.B_id;
+        
+        C_pos_i = A_pos + (B_pos-A_pos)/2;
+        i_nudge = C_pos_i + [randi([-10, 10])/10, randi([-10, 10])/10, 0];
+        dist_A = i_nudge - A_pos;
+        dist_B = i_nudge - B_pos;
+        repf = ((norm(dist_A)).^-2).*(dist_A/norm(dist_A)) + ((norm(dist_B)).^-2).*(dist_B/norm(dist_B));
+        C_pos = ((repf)*1)+i_nudge;
+        C_id = g{i}.incrementPoints();
+        
+        first = Gene_Link(A_id, A_pos, C_id, C_pos, new_stiffness);
+        second = Gene_Link(B_id, B_pos, C_id, C_pos, new_stiffness);
+        g{length(g)+1} = first;
+        g{length(g)+1} = second; 
+        disp('Newnode Mutate')
     end
             
         
