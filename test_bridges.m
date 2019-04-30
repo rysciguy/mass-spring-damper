5cb65124db073882e6263eb4d95babc3b2ea4f65
rysciguy <rysciguy@gmail.com>
2019-04-29 20:36:05 -0600

    Testing code

--- test_bridges.m
+++ test_bridges.m
@@ -1,21 +1,17 @@
 % Script that plots and evaluates the fitness of a sample of manually
 % generated Bridge structures
-clear all %necessary to get rid 
 
-%% Settings
-x_limits = [-1, 11];
-y_limits = [-5, 5];
-limits = [x_limits, y_limits];
+clear all %necessary to get rid of persistant variables in Gene class
 
 %% Build bridge from genome
-genome0 = {Gene_Node([0, 0, 0]);
-            Gene_Node([5, 0, 0]);
-            Gene_Node([10, 0, 0]);
-            Gene_Connect(1, 2, 1);
-            Gene_Connect(2, 3, 1);
-            Gene_Nudge(2, [2,1,0]);
-            Gene_Split(1)
-            Gene_Nudge(4, [-1, -1, 0])};
+% genome0 = {Gene_Node([0, 0, 0]);
+%             Gene_Node([5, 0, 0]);
+%             Gene_Node([10, 0, 0]);
+%             Gene_Connect(1, 2, 1);
+%             Gene_Connect(2, 3, 1);
+%             Gene_Nudge(2, [2,1,0]);
+%             Gene_Split(1)
+%             Gene_Nudge(4, [-1, -1, 0])};
 
         
 genome = {Gene_Node([0, 0, 0]);
@@ -62,23 +58,9 @@ p2 = [1:8,13:32];
 f1 = 1;
 f2 = 1;
 [c1, c2] = crossover(p1, f1, p2, f2);
-sequences = {p1;p2;c1;c2};
+% sequences = {p1;p2;c1;c2};
 % 
 % sequences = {1:3;
 %              1:8};
-                
-n = size(sequences, 1);
-cols = 4;
-rows = ceil(n/cols);
-bridges = Bridge.empty(n, 0);
-displacements = zeros(n, 1);
-masses = zeros(n, 1);
-for i = 1:n
-    bridges(i) = Bridge(genome(sequences{i}));
-    bridges(i).assemble();
-    
-    arguments = {'num_plots', n, 'plot_ind', i}; %used for subplots
-    [d, m] = evaluateBridgeFitness(bridges(i), arguments{:});
-    displacements(i) = d;
-    masses(i) = d;
-end
+
+testSequences(sequences, genome);
