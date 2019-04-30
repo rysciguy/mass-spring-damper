50d8b994c7154263d8397fcbe728af82554943e2
rysciguy <rysciguy@gmail.com>
2019-04-29 21:56:27 -0600

    Added static property to Gene_Node
    If obj.static=true, then the node should be exempt from mutation

--- Genes/Gene_Node.m
+++ Genes/Gene_Node.m
@@ -2,10 +2,19 @@ classdef Gene_Node < Gene
     properties
         pos;
         pt_id;
+        static = false;
     end
     
     methods
-        function obj = Gene_Node(pos)
+        function obj = Gene_Node(pos, varargin)
+            for index = 1:2:nargin-1
+                if nargin-1==index, break, end
+                switch lower(varargin{index}) %key
+                    case 'static'
+                        obj.static = true;
+                end
+            end
+            
             obj.innovation = obj.incrementInnovation;
             
             obj.pos = pos;
