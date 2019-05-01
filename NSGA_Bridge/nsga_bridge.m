04045b13842faac6fa398ab80592c65e7a7a003f
rysciguy <rysciguy@gmail.com>
2019-04-30 18:14:16 -0600

    Adapting NSGA-2 for our Bridges

---
+++ NSGA_Bridge/nsga_bridge.m
@@ -0,0 +1,21 @@
+function nsga_bridge(pop, gen)
+%  Modified by Ryan Reedy (2019)
+
+% chromosome
+    % chromosome(:, 1:M)            fitness
+    % chromosome(:, M+1)            rank
+    % chromosome(:, end)            crowding
+
+M = 2; %number of objectives
+
+%% Initialize the population
+% Population is initialized with random values which are within the
+% specified range. Each chromosome consists of the decision variables. Also
+% the value of the objective functions, rank and crowding distance
+% information is also added to the chromosome vector but only the elements
+% of the vector which has the decision variables are operated upon to
+% perform the genetic operations like corssover and mutation.
+[genome, chromosome] = initialize_variables(pop, M);
+
+
+end
